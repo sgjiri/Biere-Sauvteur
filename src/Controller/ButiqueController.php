1244e737dec05af16e217a61bb57861dabc7d90b
sg.jiri <sg.jiri@yahoo.fr>
2023-10-21 16:49:22 +0200

    fistreq

--- src/Controller/ButiqueController.php
+++ src/Controller/ButiqueController.php
@@ -5,14 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProduitRepository;
 
 class ButiqueController extends AbstractController
 {
     #[Route('/butique', name: 'app_butique')]
-    public function index(): Response
-    {
-        return $this->render('page/boutique.html.twig', [
-            'controller_name' => 'ButiqueController',
-        ]);
+    public function index(ProduitRepository $produitRepository): Response
+{
+    $produits = $produitRepository->findAll();
+
+
+    foreach ($produits as $produit) {
+        $categories = $produit->getCategoryId();
+        var_dump($categories);
+
     }
+
+    // Maintenant, vous pouvez utiliser $ids comme tableau d'IDs dans votre template
+    return $this->render('page/boutique.html.twig', [
+        'controller_name' => 'ButiqueController',
+        'produits' => $produits,
+    ]);
+}
+
+
 }
--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use phpDocumentor\Reflection\PseudoTypes\Numeric_;
 
 #[ORM\Entity(repositoryClass: ProduitRepository::class)]
 class Produit
@@ -22,9 +23,6 @@ class Produit
     #[ORM\Column(type: Types::TEXT)]
     private ?string $Description = null;
 
-    #[ORM\Column]
-    private ?float $Price = null;
-
     #[ORM\Column(length: 150)]
     private ?string $Thumbnail = null;
 
@@ -33,6 +31,21 @@ class Produit
 
     #[ORM\ManyToMany(targetEntity: categories::class)]
     private Collection $category_id;
+
+    #[ORM\Column(type: "decimal", precision: 10, scale: 2)]
+    private ?float $price = null;
+
+    #[ORM\Column]
+    private ?float $alcohol = null;
+
+    #[ORM\Column]
+    private ?float $volume = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $color = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $colorhex = null;
 
     public function __construct()
     {
@@ -64,18 +77,6 @@ class Produit
     public function setDescription(string $Description): static
     {
         $this->Description = $Description;
-
-        return $this;
-    }
-
-    public function getPrice(): ?int
-    {
-        return $this->Price;
-    }
-
-    public function setPrice(int $Price): static
-    {
-        $this->Price = $Price;
 
         return $this;
     }
@@ -124,6 +125,66 @@ class Produit
     public function removeCategoryId(categories $categoryId): static
     {
         $this->category_id->removeElement($categoryId);
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getAlcohol(): ?float
+    {
+        return $this->alcohol;
+    }
+
+    public function setAlcohol(float $alcohol): static
+    {
+        $this->alcohol = $alcohol;
+
+        return $this;
+    }
+
+    public function getVolume(): ?float
+    {
+        return $this->volume;
+    }
+
+    public function setVolume(float $volume): static
+    {
+        $this->volume = $volume;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): static
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getColorHex(): ?string
+    {
+        return $this->colorhex;
+    }
+
+    public function setColorHex(string $color_hex): static
+    {
+        $this->colorhex = $color_hex;
 
         return $this;
     }
